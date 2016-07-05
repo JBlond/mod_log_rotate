bcf927e252d94b3ce09bba8ae6004294b1638e39
JBlond <leet31337@web.de>
2016-07-05 23:00:13 +0200

    correct signature

--- src/mod_log_rotate.c
+++ src/mod_log_rotate.c
@@ -412,7 +412,7 @@ static void *merge_log_options(apr_pool_t *p, void *basev, void *addv) {
 /* map into the first apache */
 static int log_rotate_post_config( apr_pool_t * p, apr_pool_t * plog, apr_pool_t * ptemp, server_rec * s)
 {
-	ap_add_version_component(p, "mod_log_rotate/1.01");
+	ap_add_version_component(p, "mod_log_rotate/1.02");
 	return OK;
 }
 
